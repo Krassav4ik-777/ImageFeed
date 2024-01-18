9a3bed61d48cc902a1b770c5db6f4f2ad13aef1a
Albert <Krassav4ik7777@yandex.ru>
2024-01-18 20:05:06 +0300

    1.Установка атлетов для даты, картинки и кнопки лайк. 2.Изменение констрейнтов LaunchScrin. 3.Переименование иконок лайка. 4.Настройка ячейки.

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -11,9 +11,7 @@ import UIKit
 final class  ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
     
-    
-    
-    
-    
-    
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
 }
