7df6716780c0b7db9e7f29ef635f48a4182ad898
Albert <Krassav4ik7777@yandex.ru>
2024-02-07 18:56:51 +0300

    1.Добавление нового класса SingleImageViewController. 2.Добавление Navigator контроллера. 3.Добавление нового экрана просмотра изображения, установка его аутлета.

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by KraSSavchiK on 06.02.2024.
+//
+
+import Foundation
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
