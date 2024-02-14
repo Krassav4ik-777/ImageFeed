8cb55eb5627a3e203f08785595a41200007898e5
Albert <Krassav4ik7777@yandex.ru>
2024-02-14 23:49:37 +0300

    1.Добавление кнопки "Поделиться". 2.Добавление алгоритма рескейла. 3.Добавление СкролВью и методы для него.

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -3,9 +3,7 @@
 //  ImageFeed
 //
 //  Created by KraSSavchiK on 06.02.2024.
-//
 
-import Foundation
 import UIKit
 
 final class SingleImageViewController: UIViewController {
@@ -13,19 +11,59 @@ final class SingleImageViewController: UIViewController {
         didSet {
             guard isViewLoaded else { return }
             imageView.image = image
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
     
     @IBOutlet private var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        imageView.image = image
+        imageView.frame.size = image.size
+        rescaleAndCenterImageInScrollView(image: image)
     }
     
+    // Кнопка возврата
     @IBAction private func didTapBackButton() {
         dismiss(animated: true,completion: nil)
     }
     
+    // Добавление шеринга кнопки
+    @IBAction func didTapShareButton(_ sender: UIButton) {
+        let share = UIActivityViewController(
+            activityItems: [image!],
+            applicationActivities: nil
+        )
+        present(share, animated: true, completion: nil)
+    }
     
+    // Алгоритм рескейла
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+}
+
+// метод сообщающий ScrollView какую Вью зумить
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
 }
